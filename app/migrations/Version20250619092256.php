327ce661e3d1196f3cb5e4f749e4dcd2383d4f91
Morvin Quernel <quernel.morvin@icloud.com>
2025-06-19 11:32:13 +0200

    env

--- app/migrations/Version20250619092256.php
+++ app/migrations/Version20250619092256.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250613142506 extends AbstractMigration
+final class Version20250619092256 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -27,6 +27,9 @@ final class Version20250613142506 extends AbstractMigration
             CREATE TABLE categories (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(50) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
         SQL);
         $this->addSql(<<<'SQL'
+            CREATE TABLE orders (id INT AUTO_INCREMENT NOT NULL, users_id INT DEFAULT NULL, status VARCHAR(50) NOT NULL, date_order DATETIME NOT NULL COMMENT '(DC2Type:datetime_immutable)', INDEX IDX_E52FFDEE67B3B43D (users_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+        SQL);
+        $this->addSql(<<<'SQL'
             CREATE TABLE orders_products (id INT AUTO_INCREMENT NOT NULL, orders_id INT DEFAULT NULL, rating INT DEFAULT NULL, quantity INT NOT NULL, price DOUBLE PRECISION NOT NULL, rating_at DATETIME DEFAULT NULL COMMENT '(DC2Type:datetime_immutable)', INDEX IDX_749C879CCFFE9AD6 (orders_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
         SQL);
         $this->addSql(<<<'SQL'
@@ -36,10 +39,19 @@ final class Version20250613142506 extends AbstractMigration
             CREATE TABLE products (id INT AUTO_INCREMENT NOT NULL, orders_products_id INT DEFAULT NULL, title VARCHAR(50) NOT NULL, price DOUBLE PRECISION NOT NULL, description VARCHAR(255) DEFAULT NULL, notice VARCHAR(255) DEFAULT NULL, INDEX IDX_B3BA5A5A1F335533 (orders_products_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
         SQL);
         $this->addSql(<<<'SQL'
+            CREATE TABLE users (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, name VARCHAR(100) NOT NULL, firstname VARCHAR(100) NOT NULL, phone_number VARCHAR(20) NOT NULL, UNIQUE INDEX UNIQ_IDENTIFIER_EMAIL (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+        SQL);
+        $this->addSql(<<<'SQL'
             CREATE TABLE users_products (users_id INT NOT NULL, products_id INT NOT NULL, INDEX IDX_C8FB718067B3B43D (users_id), INDEX IDX_C8FB71806C8A81A9 (products_id), PRIMARY KEY(users_id, products_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
         SQL);
         $this->addSql(<<<'SQL'
+            CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL COMMENT '(DC2Type:datetime_immutable)', available_at DATETIME NOT NULL COMMENT '(DC2Type:datetime_immutable)', delivered_at DATETIME DEFAULT NULL COMMENT '(DC2Type:datetime_immutable)', INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+        SQL);
+        $this->addSql(<<<'SQL'
             ALTER TABLE addresses ADD CONSTRAINT FK_6FCA751667B3B43D FOREIGN KEY (users_id) REFERENCES users (id)
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE orders ADD CONSTRAINT FK_E52FFDEE67B3B43D FOREIGN KEY (users_id) REFERENCES users (id)
         SQL);
         $this->addSql(<<<'SQL'
             ALTER TABLE orders_products ADD CONSTRAINT FK_749C879CCFFE9AD6 FOREIGN KEY (orders_id) REFERENCES orders (id)
@@ -53,15 +65,6 @@ final class Version20250613142506 extends AbstractMigration
         $this->addSql(<<<'SQL'
             ALTER TABLE users_products ADD CONSTRAINT FK_C8FB71806C8A81A9 FOREIGN KEY (products_id) REFERENCES products (id) ON DELETE CASCADE
         SQL);
-        $this->addSql(<<<'SQL'
-            ALTER TABLE orders ADD users_id INT DEFAULT NULL
-        SQL);
-        $this->addSql(<<<'SQL'
-            ALTER TABLE orders ADD CONSTRAINT FK_E52FFDEE67B3B43D FOREIGN KEY (users_id) REFERENCES users (id)
-        SQL);
-        $this->addSql(<<<'SQL'
-            CREATE INDEX IDX_E52FFDEE67B3B43D ON orders (users_id)
-        SQL);
     }
 
     public function down(Schema $schema): void
@@ -69,6 +72,9 @@ final class Version20250613142506 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql(<<<'SQL'
             ALTER TABLE addresses DROP FOREIGN KEY FK_6FCA751667B3B43D
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE orders DROP FOREIGN KEY FK_E52FFDEE67B3B43D
         SQL);
         $this->addSql(<<<'SQL'
             ALTER TABLE orders_products DROP FOREIGN KEY FK_749C879CCFFE9AD6
@@ -89,6 +95,9 @@ final class Version20250613142506 extends AbstractMigration
             DROP TABLE categories
         SQL);
         $this->addSql(<<<'SQL'
+            DROP TABLE orders
+        SQL);
+        $this->addSql(<<<'SQL'
             DROP TABLE orders_products
         SQL);
         $this->addSql(<<<'SQL'
@@ -98,16 +107,13 @@ final class Version20250613142506 extends AbstractMigration
             DROP TABLE products
         SQL);
         $this->addSql(<<<'SQL'
+            DROP TABLE users
+        SQL);
+        $this->addSql(<<<'SQL'
             DROP TABLE users_products
         SQL);
         $this->addSql(<<<'SQL'
-            ALTER TABLE orders DROP FOREIGN KEY FK_E52FFDEE67B3B43D
-        SQL);
-        $this->addSql(<<<'SQL'
-            DROP INDEX IDX_E52FFDEE67B3B43D ON orders
-        SQL);
-        $this->addSql(<<<'SQL'
-            ALTER TABLE orders DROP users_id
+            DROP TABLE messenger_messages
         SQL);
     }
 }
